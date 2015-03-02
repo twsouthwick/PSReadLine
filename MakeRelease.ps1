19d6eeea74f78ffc9b9a5bf28c4ad58866e548bd
Taylor Southwick <twsouthwick@outlook.com>
2015-03-02 12:06:39 -0800

    Rename to psreadline-vim

--- MakeRelease.ps1
+++ MakeRelease.ps1
@@ -1,15 +1,18 @@
-param([switch]$Install, [switch]$BuildChocolatey)
-
-add-type -AssemblyName System.IO.Compression.FileSystem
+if (-not(Get-Command -Name nuget -ErrorAction Ignore))
+{
+    Write-Warning "Cannot find nuget"
+	exit
+}
 
 if (-not(Get-Command -Name msbuild -ErrorAction Ignore))
 {
-    $env:path += ";${env:SystemRoot}\Microsoft.Net\Framework\v4.0.30319"
+	Write-Warning "Cannot find msbuild"
+	exit
 }
 
 msbuild $PSScriptRoot\PSReadline\PSReadLine.sln /t:Rebuild /p:Configuration=Release
 
-$targetDir = "${env:Temp}\PSReadline"
+$targetDir = "${env:Temp}\PSReadline-vim"
 
 if (Test-Path -Path $targetDir)
 {
@@ -22,8 +25,8 @@ $null = mkdir $targetDir\en-US
 $files = @('PSReadline\Changes.txt',
            'PSReadline\License.txt',
            'PSReadline\SamplePSReadlineProfile.ps1',
-           'PSReadline\PSReadline.psd1',
-           'PSReadline\PSReadline.psm1',
+           'PSReadline\PSReadline-vim.psd1',
+           'PSReadline\PSReadline-vim.psm1',
            'PSReadline\PSReadline.format.ps1xml',
            'PSReadline\bin\Release\PSReadline.dll')
 
@@ -40,49 +43,10 @@ foreach ($file in $files)
     copy $PSScriptRoot\$file $targetDir\en-us
 }
 
-$version = (Get-ChildItem -Path $targetDir\PSReadline.dll).VersionInfo.FileVersion
-
-& $PSScriptRoot\Update-ModuleManifest.ps1 $targetDir\PSReadline.psd1 $version
-
 #make sure chocolatey is installed and in the path
-if ($BuildChocolatey -and (Get-Command -Name cpack -ErrorAction Ignore))
-{
-    $chocolateyDir = "$PSScriptRoot\ChocolateyPackage"
+#if ($BuildChocolatey -and (Get-Command -Name cpack -ErrorAction Ignore))
 
-    if (Test-Path -Path $chocolateyDir\PSReadline)
-    {
-        rmdir -Recurse $chocolateyDir\PSReadline
-    }
+copy $PSScriptRoot\PSReadline-vim.nuspec $targetDir
 
-    & $PSScriptRoot\Update-NuspecVersion.ps1 "$chocolateyDir\PSReadline.nuspec" $version
+nuget pack "$targetDir\PSReadline-vim.nuspec" -NonInteractive -NoPackageAnalysis
 
-    copy -Recurse $targetDir $chocolateyDir\PSReadline
-
-    cpack "$chocolateyDir\PSReadline.nuspec"
-}
-
-del $PSScriptRoot\PSReadline.zip -ErrorAction Ignore
-[System.IO.Compression.ZipFile]::CreateFromDirectory($targetDir, "$PSScriptRoot\PSReadline.zip")
-
-if ($Install)
-{
-    $InstallDir = "$HOME\Documents\WindowsPowerShell\Modules"
-
-    if (-not(Test-Path -Path $InstallDir))
-    {
-        mkdir -force $InstallDir
-    }
-
-    try
-    {
-        if (Test-Path -Path $InstallDir\PSReadline)
-        {
-            rmdir -Recurse -force $InstallDir\PSReadline -ErrorAction Stop
-        }
-        copy -Recurse $targetDir $InstallDir
-    }
-    catch
-    {
-        Write-Error -Message "Can't install, module is probably in use."
-    }
-}
